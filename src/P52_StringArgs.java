f48c766f95ff5dad36f5642873ff2bff84f958e2
appifybrands <appifybrands@gmail.com>
2026-01-16 19:34:07 +0530

    string args

---
+++ src/P52_StringArgs.java
@@ -0,0 +1,7 @@
+public class P52_StringArgs{
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        System.out.println("First Argument: "+ args[0]);
+        System.out.println("Second Argument: "+ args[1]);
+    }
+}
